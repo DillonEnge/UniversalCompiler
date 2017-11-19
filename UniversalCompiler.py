887289b9020d9220d79b00d68fa131c6ae6417cb
Dillon Enge <dillon.enge@gmail.com>
2017-11-19 00:44:38 -0500

    Wrote a fully working alpha

--- UniversalCompiler.py
+++ UniversalCompiler.py
@@ -8,14 +8,46 @@ class FileManipulator():
         abs_file_path = os.path.join(script_dir, rel_path)
         return abs_file_path
 
-    def readInFile(self, path, outputType):
-        file = open(self.getAbsPath(path), "r+")
-        self.interperateFile(file, outputType)
+    def transformUPFile(self):
+        path = "test.up"#raw_input("What is the full name of your uni file? ")
+        outputType = raw_input("What is your desired file type? ")
+        uniFile = open(self.getAbsPath(path), "r")
+        delimitedFile = csv.reader(uniFile, delimiter=" ")
+        self.interperateFile(delimitedFile, outputType)
 
-    def interperateFile(self, file, outputType):
-        absPath = getAbsPath("comparator.csv")
-        csvFile = open(absPath, "r+")
-        comparisonFile = csv.reader(csvFile, delimiter=" ")
+    def interperateFile(self, uniFile, outputType):
+        csvFile = open(self.getAbsPath("comparator.csv"), "r")
+        comparisonFile = csv.reader(csvFile, delimiter=",")
+        outputFileData = self.getRelevantData(uniFile, comparisonFile, outputType)
+        self.createNewFile(outputFileData, outputType)
+        
 
+    def createNewFile(self, outputFileData, outputType):
+        fileName = raw_input("What would you like your {} file to be called? ".format(outputType))
+        fileName = self.getAbsPath(fileName + "." + outputType)
+        outputFile = open(fileName, "w")
+        for output in outputFileData:
+            outputFile.write(output)
+        outputFile.close()
+
+    def getRelevantData(self, uniFile, comparisonFile, outputType):
+        relevantData = []
+        for row in comparisonFile:
+            if row[0] == outputType:
+                del row[0]
+                relevantData.append(row)
+        outputFile = []
+        for line in uniFile:
+            outputFile.append("\n")
+            for keyword in line:
+                for row in relevantData:
+                    if keyword == row[0]:
+                        if keyword == line[len(line) - 1]:
+                            outputFile.append(row[1])
+                        else:
+                            outputFile.append(row[1] + " ")
+        del outputFile[0]
+        return outputFile
+        
 fm = FileManipulator()
-fm.readInFile("test.up", "js")
+fm.transformUPFile()
